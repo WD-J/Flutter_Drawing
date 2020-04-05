084b2dee80a3ce31d4eb65401f56938125252f15
tavuskusu <48766285+tavuskusu@users.noreply.github.com>
2020-04-05 16:52:03 +0300

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -12,12 +12,12 @@ class HomePage extends StatefulWidget {
 
 class _HomePageState extends State<HomePage> {
   List<Offset> _points = <Offset>[];
-  List<Offset> _pointsSaved = <Offset>[];
-  List<Offset> _pointsSaved2 = <Offset>[];
-  //List<List<Offset>> _pointList= [];
-  var _pointList = new List<List<Offset>>(50);
+
+  List<List<Offset>>_pointList = List<List<Offset>>(50);
+  
   int i = 0;
   int j = 0;
+  bool firstload = true;
 
   @override
   Widget build(BuildContext context) {
@@ -35,73 +35,35 @@ class _HomePageState extends State<HomePage> {
                     _points = new List.from(_points)..add(_localPosition);
                   });
                 },
-                onPanEnd: (DragEndDetails details) => _points.add(null),
-                child: new CustomPaint(
-                  painter: new Signature(points: _points),
-                  size: Size.fromWidth(MediaQuery.of(context).size.width / 2),
-                ),
-              ),
-            ),
-            new Container(
-              child: new GestureDetector(
-                onPanUpdate: (DragUpdateDetails details) {
-                  setState(() {
-                    RenderBox object = context.findRenderObject();
-                    Offset _localPosition =
-                        object.globalToLocal(details.globalPosition);
-                    _points = new List.from(_points)..add(_localPosition);
-                  });
-                },
                 onPanEnd: (DragEndDetails details) {
                   setState(() {
                     _points.add(null);
-                    _pointList[i] = _points;
                     i++;
+                    _pointList[i+1] = _points;
+                    //print(i);
+                    
                   });
                 },
                 child: new CustomPaint(
                   painter: new Signature(points: _points),
-                  size: Size.fromWidth(MediaQuery.of(context).size.width / 2),
+                  size: Size.fromWidth(MediaQuery.of(context).size.width),
                 ),
               ),
             ),
           ],
         ),
         Positioned(
-          bottom: 10,
-          left: 10,
-          child: RaisedButton(
-            child: Text("1.save"),
-            onPressed: () {
-              setState(() {
-                _pointList[i] = _points;
-                i++;
-              });
-            },
-          ),
-        ),
-        Positioned(
           top: 10,
           left: 10,
           child: RaisedButton(
-            child: Text("1.load"),
+            child: Text("Load"),
             onPressed: () {
               setState(() {
-                _points = _pointList[j];
+                _pointList[0]= _points;
                 j++;
-              });
-            },
-          ),
-        ),
-        Positioned(
-          top: 10,
-          right: 10,
-          child: RaisedButton(
-            child: Text("2.load"),
-            onPressed: () {
-              setState(() {
-                //j++;
-                _points = _pointsSaved2;
+                _points = _pointList[j];
+                //print(j);
+                
               });
             },
           ),
@@ -110,6 +72,7 @@ class _HomePageState extends State<HomePage> {
       floatingActionButton: new FloatingActionButton(
         child: new Icon(Icons.clear),
         onPressed: () {
+          
           _points.clear();
         },
       ),
